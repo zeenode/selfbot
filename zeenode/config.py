e6aefad52ca028dc88599a32752e757f4b27740a
flowitoo <flowito@protonmail.com>
2021-02-11 22:26:57 +0100

    flow committing...

--- zeenode/config.py
+++ zeenode/config.py
@@ -1,3 +1,3 @@
 class config:
-    global auth; auth = "MjY3MDM2MDk4NzE3NDE3NDkz.YCWVJQ.6Dnp8ztQOrb1hfXY6h3aBJYh3ug" # Enter your discord token for Auto-Login
+    global auth; auth = "MjYgasGASGSASGAGA" # Enter your discord token for Auto-Login
     global prefix; prefix = "!" # Change prefix if you want to
