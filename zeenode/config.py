2793554c8855343a2c56b02915c5b1e1d3084323
zeenode ceo <60113773+owersite@users.noreply.github.com>
2021-02-13 00:23:29 +0100

    Delete zeenode directory

--- zeenode/config.py
+++
@@ -1,5 +0,0 @@
-class config:
-    global auth; auth = "Your Token" # Enter your discord token for Auto-Login
-    global prefix; prefix = "$" # Change prefix if you want to
-    global nitro_sniper; nitro_sniper = "true" # 'true' to enable nitro sniper, 'false' to disable
-    global giveaway_sniper; giveaway_sniper = "a" # 'true' to enable giveaway sniper, 'false' to disable
