f0e62960f7ed49113485128764fbe98225a7f25c
zeenode ceo <60113773+owersite@users.noreply.github.com>
2021-02-12 01:12:33 +0100

    Update config.py

--- zeenode/config.py
+++ zeenode/config.py
@@ -1,3 +1,3 @@
 class config:
     global auth; auth = "T O K E N" # Enter your discord token for Auto-Login
-    global prefix; prefix = "!" # Change prefix if you want to
+    global prefix; prefix = "+" # Change prefix if you want to
