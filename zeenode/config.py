8f1591b1520e3838786274758809ab1c032c9804
zeenode ceo <60113773+owersite@users.noreply.github.com>
2021-02-13 20:26:20 +0100

    Update config.py

--- zeenode/config.py
+++ zeenode/config.py
@@ -2,4 +2,4 @@ class config:
     global auth; auth = "T O K E N           H E R E" # Enter your discord token for Auto-Login
     global prefix; prefix = "$" # Change prefix if you want to
     global nitro_sniper; nitro_sniper = "true" # 'true' to enable nitro sniper, 'false' to disable
-    global giveaway_sniper; giveaway_sniper = "a" # 'true' to enable giveaway sniper, 'false' to disable
+    global giveaway_sniper; giveaway_sniper = "false" # 'true' to enable giveaway sniper, 'false' to disable
