4c283d1c18d5d6e4de0679806cc60e6ff661fe92
zeenode ceo <60113773+owersite@users.noreply.github.com>
2021-02-13 12:26:01 +0100

    Update launch.py

--- launch.py
+++ launch.py
@@ -1,3 +1,7 @@
+import os
 from zeenode.load import load
 
+window = 'mode 90,19'
+os.system(window)
 load()
+""" Ne znam kako i da li ovo radi na linuxu ali na windowsu je cmd velicina 90 i 19 --owersite """
