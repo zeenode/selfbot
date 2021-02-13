6dbb268c3b11f5db4794081847e66badc639f36f
zeenode ceo <60113773+owersite@users.noreply.github.com>
2021-02-13 20:25:30 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+class config:
+    global auth; auth = "T O K E N           H E R E" # Enter your discord token for Auto-Login
+    global prefix; prefix = "$" # Change prefix if you want to
+    global nitro_sniper; nitro_sniper = "true" # 'true' to enable nitro sniper, 'false' to disable
+    global giveaway_sniper; giveaway_sniper = "a" # 'true' to enable giveaway sniper, 'false' to disable
