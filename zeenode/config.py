45fe4e416d538761281e4f3f88abade317f30e76
owersite <60113773+owersite@users.noreply.github.com>
2021-02-18 23:18:32 +0100

    Add files via upload

---
+++ zeenode/config.py
@@ -0,0 +1,5 @@
+class config:
+    global auth; auth = "NYOUR TOKEN" # Enter your discord token for Auto-Login.
+    global prefix; prefix = "$" # Enter your prefix for selfbot.
+    global nitro_sniper; nitro_sniper = "true" # 'true' to enable nitro sniper, 'false' to disable.
+    global giveaway_sniper; giveaway_sniper = "true" # 'true' to enable giveaway sniper, 'false' to disable.
