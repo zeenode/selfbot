6574fa13e4de50c11f84f4806f37b0a579af2a74
owersite <60113773+owersite@users.noreply.github.com>
2021-02-18 22:55:08 +0100

    Update mass.py

--- zeenode/zeenode/cogs/mass.py
+++ zeenode/zeenode/cogs/mass.py
@@ -12,6 +12,13 @@ class mass(zeenode.Cog):
         messages = await ctx.message.channel.history(limit=20).flatten()
         for message in messages:
             await message.add_reaction(emote)
-
+            
+            
+            
+    @zeenode.command()
+    async def spam(self, ctx, amount:int=None, *, message: str=None):
+        await ctx.message.delete()
+        for each in range (0, amount):
+            await ctx.send(f"{message}")
 def setup(bot):
     bot.add_cog(mass(bot))
