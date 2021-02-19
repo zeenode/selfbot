65799da6b5a8c3fbb517d1f83e9ca21692d62247
owersite <60113773+owersite@users.noreply.github.com>
2021-02-19 16:01:04 +0100

    Update config.py

--- zeenode/zeenode/config.py
+++ zeenode/zeenode/config.py
@@ -1,5 +1,5 @@
 class config:
     global auth; auth = "TOKEN HERE" # Enter your discord token for Auto-Login.
-    global prefix; prefix = "YOUR PREFIX" # Enter your prefix for selfbot.
+    global prefix; prefix = "$" # Enter your prefix for selfbot.
     global nitro_sniper; nitro_sniper = "true" # 'true' to enable nitro sniper, 'false' to disable.
     global giveaway_sniper; giveaway_sniper = "true" # 'true' to enable giveaway sniper, 'false' to disable.
