9a110e6f0bc55510de0eaa79eaf694b5d07c2fd1
owersite <60113773+owersite@users.noreply.github.com>
2021-02-19 02:06:49 +0100

    Delete zeenode directory

--- zeenode/launch.py
+++
@@ -1,7 +0,0 @@
-import os
-from zeenode.load import load
-
-window = 'mode 90,19'
-os.system(window)
-load()
-""" Ne znam kako i da li ovo radi na linuxu ali na windowsu je cmd velicine 90 i 19 --owersite """
