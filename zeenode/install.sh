0464e837ef8550445173aced89d02db7576fda92
equals <equalsgit@tutanota.com>
2021-03-18 19:06:34 +0100

    Delete zeenode directory
    OwO whats this

--- zeenode/install.sh
+++
@@ -1,7 +0,0 @@
-export p="$(command -v pip)"
-if [ ! -f "$p" ]; then
-    	echo "please install pip before running"
-else
-	$p install -r requirements.txt
-fi
-
--- zeenode/launch.py
+++
@@ -1,6 +0,0 @@
-import os
-from zeenode.load import load
-
-window = 'mode 90,19'
-os.system(window)
-load()
